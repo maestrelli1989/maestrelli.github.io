e61cd777727069fd03b4469e68f4f4ceb5c527fc
Serge Tiutin <aerofan@ukr.net>
2019-11-30 12:37:41 +0200

    add uapay

---
+++ uapay/js/common.js
@@ -0,0 +1,11 @@
+/*
+	Javascript code for the UaPay website project
+	Version: 1.0
+	Created: 30.11.2019
+	Author: Serhii Tiutin
+	Modified by: Serhii Tiutin
+	Owner: UaPay
+	Website: https://uapay.ua/
+*/
+
+$(function() {});
