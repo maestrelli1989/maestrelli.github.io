70e5c8d42c75a73469cb5ebdc9082b33f6f4e870
Serge Tiutin <aerofan@ukr.net>
2019-12-16 13:39:47 +0200

    update uapay

--- uapay/js/common.js
+++ uapay/js/common.js
@@ -8,4 +8,61 @@
 	Website: https://uapay.ua/
 */
 
-$(function() {});
+$(() => {
+
+	/*
+		List of variables
+	*/
+	
+	const primaryTags = $("html, body");
+
+	const firstTrigger = $("#first-trigger");
+	const firstBlock = $("#first-block");
+
+	const secondTrigger = $("#second-trigger");
+	const secondBlock = $("#second-block");
+
+	const thirdTrigger = $("#third-trigger");
+	const thirdBlock = $("#third-block");
+
+	const fourthTrigger = $("#fourth-trigger");
+	const fourthBlock = $("#fourth-block");
+
+	const fifthTrigger = $("#fifth-trigger");
+	const fifthBlock = $("#fifth-block");
+	
+	/*
+		Smooth scroll effect
+	*/
+
+	$(firstTrigger).click(() => {
+		$(primaryTags).animate({
+			scrollTop: $(firstBlock).offset().top
+		}, 700);
+	});
+	
+	$(secondTrigger).click(() => {
+		$(primaryTags).animate({
+			scrollTop: $(secondBlock).offset().top
+		}, 700);
+	});
+
+	$(thirdTrigger).click(() => {
+		$(primaryTags).animate({
+			scrollTop: $(thirdBlock).offset().top
+		}, 700);
+	});
+
+	$(fourthTrigger).click(() => {
+		$(primaryTags).animate({
+			scrollTop: $(fourthBlock).offset().top
+		}, 700);
+	});
+
+	$(fifthTrigger).click(() => {
+		$(primaryTags).animate({
+			scrollTop: $(fifthBlock).offset().top
+		}, 700);
+	});
+
+});
