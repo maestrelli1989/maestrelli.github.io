c169620a2226f151354a3f647861bbd62953cd00
Serge Tiutin <aerofan@ukr.net>
2019-12-20 14:24:15 +0200

    update uapay

--- uapay/js/common.js
+++ uapay/js/common.js
@@ -11,28 +11,67 @@
 $(() => {
 
 	/*
-		List of variables
+		List of global constants
 	*/
 	
 	const primaryTags = $("html, body");
 
+	// Header
+	const businessMenuItemTrigger = $("#business-trigger");
+	const businessSubMenu = $("#business-sub-menu");
+	const userMenuItemTrigger = $("#user-trigger");
+	const userSubMenu = $("#user-sub-menu");
+	const companyMenuItemTrigger = $("#company-trigger");
+	const companySubMenu = $("#company-sub-menu");
+	const developersMenuItemTrigger = $("#developers-trigger");
+	const developersSubMenu = $("#developers-sub-menu");
+
+	// Scrolling block at the "Industry solutions" page
 	const firstTrigger = $("#first-trigger");
 	const firstBlock = $("#first-block");
-
 	const secondTrigger = $("#second-trigger");
 	const secondBlock = $("#second-block");
-
 	const thirdTrigger = $("#third-trigger");
 	const thirdBlock = $("#third-block");
-
 	const fourthTrigger = $("#fourth-trigger");
 	const fourthBlock = $("#fourth-block");
-
 	const fifthTrigger = $("#fifth-trigger");
 	const fifthBlock = $("#fifth-block");
-	
+
 	/*
-		Smooth scroll effect
+		Toggle effect at the Header sub menu
+	*/
+
+	$(businessMenuItemTrigger).click(() => {
+		$(userSubMenu).slideUp(100);
+		$(companySubMenu).slideUp(100);
+		$(developersSubMenu).slideUp(100);
+		$(businessSubMenu).slideToggle(500);
+	});
+
+	$(userMenuItemTrigger).click(() => {
+		$(businessSubMenu).slideUp(100);
+		$(companySubMenu).slideUp(100);
+		$(developersSubMenu).slideUp(100);
+		$(userSubMenu).slideToggle(500);
+	});
+
+	$(companyMenuItemTrigger).click(() => {
+		$(businessSubMenu).slideUp(100);
+		$(userSubMenu).slideUp(100);
+		$(developersSubMenu).slideUp(100);
+		$(companySubMenu).slideToggle(500);
+	});
+	
+	$(developersMenuItemTrigger).click(() => {
+		$(businessSubMenu).slideUp(100);
+		$(userSubMenu).slideUp(100);
+		$(companySubMenu).slideUp(100);
+		$(developersSubMenu).slideToggle(500);
+	});
+
+	/*
+		Smooth scroll effect at the "Industry solutions" page
 	*/
 
 	$(firstTrigger).click(() => {
